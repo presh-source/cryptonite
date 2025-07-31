01d0c314fc2bdb9f319d4a341b0710cea8cc5740
precious.opeyemi <opeyemiprecious16@gmail.com>
2025-07-31 19:17:48 +0100

    Initial commit: Strong password checker in Java

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,72 @@
 class StrongPasswordV1 {
-    private String password;
+    private final String password;
+    private final int minimumLength;
 
-    public static void main(String password) {
-        boolean noSpace = false;
-        boolean minimumLength = false;
-        boolean upperCase = false;
-        boolean lowerCase = false;
-        boolean number = false;
-        boolean special = false;
-        char[] passwordChars = password.toCharArray();
-
-        noSpace = password.isBlank();
-        minimumLength = password.length() >= 8;
-
-        upperCase = containsUpperCase(password);
-        lowerCase = containsLowerCase(password);
-
+    public StrongPasswordV1(String password, int minimumLength) {
+        this.password = password;
+        this.minimumLength = minimumLength;
     }
 
-    private static boolean containsUpperCase(String text) {
-        for (char c : text.toCharArray()) {
+    public boolean hasMinimumLength() {
+        return password.length() >= minimumLength;
+    }
+
+    public boolean containsSpace() {
+        return password.indexOf(' ') >= 0;
+    }
+
+    public boolean containsUpperCase() {
+        for (char c : password.toCharArray()) {
             if (Character.isUpperCase(c)) return true;
         }
         return false;
     }
 
-    private static boolean containsLowerCase(String text) {
-        for (char c : text.toCharArray()) {
+    public boolean containsLowerCase() {
+        for (char c : password.toCharArray()) {
             if (Character.isLowerCase(c)) return true;
         }
         return false;
     }
-}
 
-public class Main {
+    public boolean containsDigit() {
+        for (char c : password.toCharArray()) {
+            if (Character.isDigit(c)) return true;
+        }
+        return false;
+    }
+
+    public boolean containsSpecialCharacter() {
+        for (char c : password.toCharArray()) {
+            if (!Character.isLetterOrDigit(c) && !Character.isWhitespace(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isStrong() {
+        return hasMinimumLength()
+                && !containsSpace()
+                && containsUpperCase()
+                && containsLowerCase()
+                && containsDigit()
+                && containsSpecialCharacter();
+    }
+
+    // Added main() so you can run this class directly
     public static void main(String[] args) {
+        String password = args.length > 0 ? args[0] : "MyPassword1";
+        int minimumLength = args.length > 1 ? Integer.parseInt(args[1]) : 8;
 
+        StrongPasswordV1 checker = new StrongPasswordV1(password, minimumLength);
+
+        System.out.println("Has minimum length: " + checker.hasMinimumLength());
+        System.out.println("Contains space: " + checker.containsSpace());
+        System.out.println("Has upper case: " + checker.containsUpperCase());
+        System.out.println("Has lower case: " + checker.containsLowerCase());
+        System.out.println("Has digit: " + checker.containsDigit());
+        System.out.println("Has special character: " + checker.containsSpecialCharacter());
+        System.out.println("Is overall strong: " + checker.isStrong());
     }
 }
